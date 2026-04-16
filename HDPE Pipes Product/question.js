2b133e17e11c404d95b779a289083f6ffb0cc116
sangarii <114224506+sangarii@users.noreply.github.com>
2026-04-16 19:33:47 +0530

    Delete HDPE Pipes Product directory

--- HDPE Pipes Product/question.js
+++
@@ -1,9 +0,0 @@
-//question active deactive event
-const buttons = document.querySelectorAll(".toggle-btn");
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const card = btn.closest(".question-card"); 
-    card.classList.toggle("active");
-  });
-});
