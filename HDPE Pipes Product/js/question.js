fe9a1b5162cb357fff9c5568aab0a61e6e6ee98e
sangarii <114224506+sangarii@users.noreply.github.com>
2026-04-16 19:36:16 +0530

    Add files via upload

---
+++ HDPE Pipes Product/js/question.js
@@ -0,0 +1,9 @@
+//question active deactive event
+const buttons = document.querySelectorAll(".toggle-btn");
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const card = btn.closest(".question-card"); 
+    card.classList.toggle("active");
+  });
+});
